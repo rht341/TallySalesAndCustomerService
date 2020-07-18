e8c82e2c18a75233e0e8d9a5417943f2d0648539
Richard Thomas <rht341@gmail.com>
2020-07-18 11:11:16 -0400

    All working.

--- TallySalesAndCustomerService/MainForm.Designer.cs
+++ TallySalesAndCustomerService/MainForm.Designer.cs
@@ -34,8 +34,12 @@
             this.label2 = new System.Windows.Forms.Label();
             this.salesCountTextBox = new System.Windows.Forms.TextBox();
             this.csCountTextBox = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.resetButton = new System.Windows.Forms.Button();
+            this.salesCountDateLabel = new System.Windows.Forms.Label();
+            this.csCountDateLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonPressTimeoutTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // salesButton
@@ -43,18 +47,19 @@
             this.salesButton.BackColor = System.Drawing.Color.Red;
             this.salesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.salesButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.salesButton.Location = new System.Drawing.Point(142, 119);
+            this.salesButton.Location = new System.Drawing.Point(49, 43);
             this.salesButton.Name = "salesButton";
             this.salesButton.Size = new System.Drawing.Size(211, 93);
             this.salesButton.TabIndex = 0;
             this.salesButton.Text = "Sales";
             this.salesButton.UseVisualStyleBackColor = false;
+            this.salesButton.Click += new System.EventHandler(this.salesButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(139, 237);
+            this.label1.Location = new System.Drawing.Point(46, 161);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 16);
             this.label1.TabIndex = 6;
@@ -65,18 +70,19 @@
             this.csButton.BackColor = System.Drawing.Color.Red;
             this.csButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.csButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.csButton.Location = new System.Drawing.Point(447, 119);
+            this.csButton.Location = new System.Drawing.Point(354, 43);
             this.csButton.Name = "csButton";
             this.csButton.Size = new System.Drawing.Size(211, 93);
             this.csButton.TabIndex = 7;
             this.csButton.Text = "Customer Service";
             this.csButton.UseVisualStyleBackColor = false;
+            this.csButton.Click += new System.EventHandler(this.csButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(444, 237);
+            this.label2.Location = new System.Drawing.Point(351, 161);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 16);
             this.label2.TabIndex = 8;
@@ -85,7 +91,7 @@
             // salesCountTextBox
             // 
             this.salesCountTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.salesCountTextBox.Location = new System.Drawing.Point(215, 302);
+            this.salesCountTextBox.Location = new System.Drawing.Point(122, 226);
             this.salesCountTextBox.Name = "salesCountTextBox";
             this.salesCountTextBox.ReadOnly = true;
             this.salesCountTextBox.Size = new System.Drawing.Size(64, 35);
@@ -96,7 +102,7 @@
             // csCountTextBox
             // 
             this.csCountTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.csCountTextBox.Location = new System.Drawing.Point(520, 302);
+            this.csCountTextBox.Location = new System.Drawing.Point(427, 226);
             this.csCountTextBox.Name = "csCountTextBox";
             this.csCountTextBox.ReadOnly = true;
             this.csCountTextBox.Size = new System.Drawing.Size(64, 35);
@@ -104,36 +110,83 @@
             this.csCountTextBox.Text = "0";
             this.csCountTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(358, 454);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(64, 35);
-            this.textBox1.TabIndex = 12;
-            this.textBox1.Text = "0";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            // 
             // resetButton
             // 
             this.resetButton.BackColor = System.Drawing.Color.Red;
             this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.resetButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.resetButton.Location = new System.Drawing.Point(339, 293);
+            this.resetButton.Location = new System.Drawing.Point(246, 217);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(116, 56);
             this.resetButton.TabIndex = 11;
             this.resetButton.Text = "Reset";
             this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // salesCountDateLabel
+            // 
+            this.salesCountDateLabel.AutoSize = true;
+            this.salesCountDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salesCountDateLabel.Location = new System.Drawing.Point(124, 161);
+            this.salesCountDateLabel.Name = "salesCountDateLabel";
+            this.salesCountDateLabel.Size = new System.Drawing.Size(41, 16);
+            this.salesCountDateLabel.TabIndex = 13;
+            this.salesCountDateLabel.Text = "None";
+            // 
+            // csCountDateLabel
+            // 
+            this.csCountDateLabel.AutoSize = true;
+            this.csCountDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.csCountDateLabel.Location = new System.Drawing.Point(429, 161);
+            this.csCountDateLabel.Name = "csCountDateLabel";
+            this.csCountDateLabel.Size = new System.Drawing.Size(41, 16);
+            this.csCountDateLabel.TabIndex = 14;
+            this.csCountDateLabel.Text = "None";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Button Press Timeout (seconds): ";
+            // 
+            // buttonPressTimeoutTextBox
+            // 
+            this.buttonPressTimeoutTextBox.Location = new System.Drawing.Point(271, 314);
+            this.buttonPressTimeoutTextBox.Name = "buttonPressTimeoutTextBox";
+            this.buttonPressTimeoutTextBox.Size = new System.Drawing.Size(100, 20);
+            this.buttonPressTimeoutTextBox.TabIndex = 16;
+            this.buttonPressTimeoutTextBox.Text = "30";
+            this.buttonPressTimeoutTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(411, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 34);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Set Timeout";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(611, 382);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonPressTimeoutTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.csCountDateLabel);
+            this.Controls.Add(this.salesCountDateLabel);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.csCountTextBox);
             this.Controls.Add(this.salesCountTextBox);
@@ -144,6 +197,8 @@
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sales and Customer Support Counts";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,8 +212,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox salesCountTextBox;
         private System.Windows.Forms.TextBox csCountTextBox;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label salesCountDateLabel;
+        private System.Windows.Forms.Label csCountDateLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox buttonPressTimeoutTextBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- TallySalesAndCustomerService/MainForm.cs
+++ TallySalesAndCustomerService/MainForm.cs
@@ -1,20 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TallySalesAndCustomerService
 {
     public partial class MainForm : Form
     {
+        public int SalesCount { get; set; }
+        public int CSCount { get; set; }
+
+        private readonly static Timer salesTimer = new Timer();
+        private readonly static Timer csTimer = new Timer();
+
+        //const int COUNTDOWNTIME = 5000;
+
+        public int ButtonPressCountDownTime { get; set; }
         public MainForm()
         {
+            salesTimer.Tick += SalesTimerEventProcessor;
+            //salesTimer.Interval = COUNTDOWNTIME;
+            csTimer.Tick += csTimerEventProcessor;
+            //csTimer.Interval = COUNTDOWNTIME;
+
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            int buttonPressCountDownTime = Int32.Parse(buttonPressTimeoutTextBox.Text) * 1000;
+            salesTimer.Interval = buttonPressCountDownTime;
+            csTimer.Interval = buttonPressCountDownTime;
+
+            ResetValues();
+        }
+
+        private void SalesTimerEventProcessor(object sender, EventArgs e)
+        {
+            salesTimer.Stop();
+
+            salesButton.Enabled = true;
+            salesButton.BackColor = Color.Red;
+        }
+
+        private void csTimerEventProcessor(object sender, EventArgs e)
+        {
+            csTimer.Stop();
+
+            csButton.Enabled = true;
+            csButton.BackColor = Color.Red;
+        }
+        private void ResetValues()
+        {
+            salesCountDateLabel.Text = "None";
+            salesCountTextBox.Text = "0";
+            csCountDateLabel.Text = "None";
+            csCountTextBox.Text = "0";
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to reset?",
+                                                  "Reset?",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                ResetValues();
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit? All values will be lost.",
+                                                  "Exit?",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Question);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void salesButton_Click(object sender, EventArgs e)
+        {
+            SalesCount++;
+
+            salesCountTextBox.Text = SalesCount.ToString();
+            salesCountDateLabel.Text = DateTime.Now.ToString();
+
+            salesButton.Enabled = false;
+            salesButton.BackColor = Color.Gray;
+            salesTimer.Start();
+        }
+
+        private void csButton_Click(object sender, EventArgs e)
+        {
+            CSCount++;
+
+            csCountTextBox.Text = CSCount.ToString();
+            csCountDateLabel.Text = DateTime.Now.ToString();
+
+            csButton.Enabled = false;
+            csButton.BackColor = Color.Gray;
+            csTimer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int newTimeout = Int32.Parse(buttonPressTimeoutTextBox.Text) * 1000;
+
+            salesTimer.Interval = newTimeout;
+            csTimer.Interval = newTimeout;
         }
     }
 }
